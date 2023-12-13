c27c52e3977ff247176c8de83142d5170a8e12cc
Roberto <roberto.guarneros11@gmail.com>
2023-12-13 18:28:05 +0100

    Modified service to avoid error when adding matches or modyfying data

--- src/main/java/edu/upc/dsa/services/Service.java
+++ src/main/java/edu/upc/dsa/services/Service.java
@@ -28,16 +28,18 @@ public class Service {
         this.m = ManagerImpl.getInstance();
         if(m.size()==0){
             m.register("robertoguarneros11","123","Roberto","Guarneros","roberto@gmail.com","02/11/2002");
-            m.createMatch("robertoguarneros11");
-            m.endMatch("robertoguarneros11");
-            m.getUser("robertoguarneros11").setPointsEarned(500);//set 500 points so we can testAddItem
+
+            //Commented since we do not have the DDBB implementation yet
+            //m.createMatch("robertoguarneros11");
+            //m.endMatch("robertoguarneros11");
+            //m.getUser("robertoguarneros11").setPointsEarned(500);//set 500 points, so we can testAddItem
 
             m.register("titi", "456","Carles","Sanchez","titi@gmail.com","02/11/2002");
-            m.createMatch("titi");
+            //m.createMatch("titi");
             m.register("Luxu","789","Lucia","Ocaña","lucia@gmail.com","02/11/2002");
             m.register("Xuculup","000","Ángel","Redondo","angel@gmail.com","02/11/2002");
-            m.addObjectToStore("123","Poción", 100, "Poción de salto");
-            m.addObjectToStore("222","skin",50,"skin cosmetica");
+            //m.addObjectToStore("123","Poción", 100, "Poción de salto");
+            //m.addObjectToStore("222","skin",50,"skin cosmetica");
         }
     }
 
